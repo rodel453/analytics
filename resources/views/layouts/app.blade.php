d85ac438e3d34283ef31b13fb03117ee651488dc
Renon-Aquino <78833518+Renon-Aquino@users.noreply.github.com>
2022-08-15 14:34:18 +0800

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <!-- <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1"> -->
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    @include('layouts.partials.head')
+
+   
+</head>
+
+<body id="page-top">
+
+<!-- Page Wrapper -->
+<div id="wrapper">
+
+    <!-- Sidebar -->
+     @include('layouts.partials.menu') 
+    <!-- End of Sidebar -->
+
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+        @include('layouts.partials.topbar')
+
+        <!-- Main Content -->
+        <div id="content">
+
+            <!-- Topbar -->
+            <!-- End of Topbar -->
+
+            <!-- Begin Page Content -->
+            @yield('content') 
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- End of Main Content -->
+
+        <!-- Footer -->
+        @include('layouts.partials.footer') 
+        <!-- End of Footer -->
+
+    </div>
+    <!-- End of Content Wrapper -->
+
+</div>
+<!-- End of Page Wrapper -->
+
+<!-- Scroll to Top Button-->
+<a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+</a>
+
+     @include('layouts.partials.footer-script')
+
+</body>
+
+</html>
